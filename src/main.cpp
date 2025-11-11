9d86705571d86d9a646ca36d4b6d7baaf901b512
David <david.krivoklatsky@student.ssnd.sk>
2025-11-11 22:06:48 +0100

    Update weapon main implementation and add clangd files to gitignore

--- src/main.cpp
+++ src/main.cpp
@@ -1,60 +1,68 @@
-#include <Arduino.h>
+#include "ble_weapon.h"
 #include "config.h"
 #include "hash.h"
 #include "utils.h"
-#include "ble_weapon.h"
+#include <Arduino.h>
 
 BLEWeapon bleWeapon;
 
-void setup() {
-  pinMode(LASER_PIN, OUTPUT);
-  digitalWrite(LASER_PIN, LOW);
-  
-  Serial.begin(115200);
-  delay(1000);
-  
-  bleWeapon.begin();
+void setup()
+{
+    pinMode(LASER_PIN, OUTPUT);
+    digitalWrite(LASER_PIN, LOW);
+
+    Serial.begin(115200);
+    delay(1000);
+
+    bleWeapon.begin();
 }
 
-void sendBit(bool bit) {
-  digitalWrite(LASER_PIN, bit ? HIGH : LOW);
-  delay(BIT_DURATION_MS);
+void sendBit(bool bit)
+{
+    digitalWrite(LASER_PIN, bit ? HIGH : LOW);
+    delay(BIT_DURATION_MS);
 }
 
-void sendMessage(uint16_t message) {
-  for (int i = MESSAGE_TOTAL_BITS - 1; i >= 0; i--) {
-    bool bit = (message >> i) & 0x01;
-    sendBit(bit);
-  }
-  digitalWrite(LASER_PIN, LOW);
+void sendMessage(uint16_t message)
+{
+    for (int i = MESSAGE_TOTAL_BITS - 1; i >= 0; i--)
+    {
+        bool bit = (message >> i) & 0x01;
+        sendBit(bit);
+    }
+    digitalWrite(LASER_PIN, LOW);
 }
 
 uint16_t data = 0;
 
-void loop() {
-  data++;
+void loop()
+{
+    data++;
 
-  uint16_t message = createMessage16bit(data);
-  
-  // Send via BLE
-  if (bleWeapon.isConnected()) {
-    bleWeapon.sendMessage(message);
-    Serial.print("📡 BLE sent | ");
-  } else {
-    Serial.print("⚠️  BLE N/A | ");
-  }
-  
-  // Send via laser
-  sendMessage(message);
-  
-  Serial.print("► Laser | ");
-  Serial.print(millis());
-  Serial.print(" ms | ");
-  Serial.print(toBinaryString(message, MESSAGE_TOTAL_BITS));
-  Serial.print(" | Data: ");
-  Serial.println(data);
+    uint16_t message = createMessage16bit(data);
 
-  bleWeapon.handleConnection();
+    // Send via BLE
+    if (bleWeapon.isConnected())
+    {
+        bleWeapon.sendMessage(message);
+        Serial.print("📡 BLE sent | ");
+    }
+    else
+    {
+        Serial.print("⚠️  BLE N/A | ");
+    }
 
-  delay(TRANSMISSION_PAUSE_MS);
+    // Send via laser
+    sendMessage(message);
+
+    Serial.print("► Laser | ");
+    Serial.print(millis());
+    Serial.print(" ms | ");
+    Serial.print(toBinaryString(message, MESSAGE_TOTAL_BITS));
+    Serial.print(" | Data: ");
+    Serial.println(data);
+
+    bleWeapon.handleConnection();
+
+    delay(TRANSMISSION_PAUSE_MS);
 }
